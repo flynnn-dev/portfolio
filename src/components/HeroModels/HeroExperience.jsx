d787cb717e114c3d0f4982fd165fc275698305c8
Mr. Flynnn <lorensbaldon@gmail.com>
2026-06-03 15:48:51 +0800

    Fix

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -3,6 +3,7 @@ import { OrbitControls } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
 import { Room } from "./Room.jsx";
 import HeroLight from "./HeroLight.jsx";
+import Particles from "./Particles.jsx";
 
 const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
@@ -18,6 +19,7 @@ const HeroExperience = () => {
         maxPolarAngle={Math.PI / 2}
       />
       <HeroLight />
+      <Particles count={200} />
       <group
         scale={isMobile ? 0.7 : 1}
         position={[0, -3.5, 0]}
