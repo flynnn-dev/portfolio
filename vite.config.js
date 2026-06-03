2d4732aa84435e3fd455e4ecc1ede38aa56da927
Mr. Flynnn <lorensbaldon@gmail.com>
2026-06-03 14:12:06 +0800

    main

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: import.meta.env.VITE_BASE_PATH || "/portfolio/",
+  base: "/portfolio/",
 });
