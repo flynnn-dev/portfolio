425346f2e98acfe96e02f9d9e0783adf9f556121
Mr. Flynnn <lorensbaldon@gmail.com>
2026-06-03 14:07:50 +0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-})
+  plugins: [tailwindcss(), react()],
+  base: import.meta.env.VITE_BASE_PATH || "/portfolio/",
+});
