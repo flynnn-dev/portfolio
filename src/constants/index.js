46785ab2471563438792c4b8a5df9e1c019fc8ae
Mr. Flynnn <lorensbaldon@gmail.com>
2026-06-10 04:04:59 +0800

    Final

--- src/constants/index.js
+++ src/constants/index.js
@@ -251,22 +251,22 @@ const socialImgs = [
   {
     name: "insta",
     url: "https://www.instagram.com/",
-    imgPath: "/images/insta.png",
+    imgPath: "images/insta.png",
   },
   {
     name: "fb",
     url: "https://www.facebook.com/",
-    imgPath: "/images/fb.png",
+    imgPath: "images/fb.png",
   },
   {
     name: "x",
     url: "https://www.x.com/",
-    imgPath: "/images/x.png",
+    imgPath: "images/x.png",
   },
   {
     name: "linkedin",
     url: "https://www.linkedin.com/",
-    imgPath: "/images/linkedin.png",
+    imgPath: "images/linkedin.png",
   },
 ];
 
---
+++ src/sections/Contact.jsx
@@ -0,0 +1,79 @@
+import React, {useRef, useState} from 'react'
+import TitleHeader from "../components/TitleHeader.jsx";
+import ContactExperience from "../components/ContactExperience.jsx";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+    const formRef = useRef(null);
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+    const [loading, setLoading] = useState(false);
+    const  handleChange = e => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+            setLoading(true);
+
+        try {
+            await emailjs.sendForm(
+                import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+                import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+                formRef.current,
+                import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+            )
+        setFormData({name: "", email: "", message: ""});
+        }catch (error) {
+            console.log("Emailjs Error: ", error);
+        }finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <section id="contact" className="flex-center section-padding">
+            <div className="w-full h-full md:px-10 px-5">
+                <TitleHeader title="Get In Touch With Me" sub="✉️ Contact Information" />
+                <div className="mt-16 grid-12-cols">
+                    <div className="xl:col-span-5 ">
+                    <div className="flex-center card-border rounded-xl p-10">
+                    <form onSubmit={handleSubmit} className="w-full flex flex-col gap-7" ref={formRef}>
+                        <div className="mb-6">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" id="name" name="name" placeholder="Your name" onChange={handleChange} value={formData.name} required />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="email">Email</label>
+                            <input type="text" id="email" name="email" placeholder="Your email address " onChange={handleChange} value={formData.email} required />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="message">Message</label>
+                            <textarea rows="5" id="message" name="message" placeholder="Your message" onChange={handleChange} value={formData.message} required />
+                        </div>
+                            <button type="submit" disabled={loading}>
+                                <div className="cta-button group">
+                                    <div className="bg-circle"/>
+                                    <p className="text">{loading ? "Sending..." : "Send Message"}</p>
+                                    <div className="arrow-wrapper">
+                                        <img src="images/arrow-down.svg" alt="arrow"/>
+                                    </div>
+                                </div>
+                            </button>
+                    </form>
+                    </div>
+                    </div>
+                    <div className="xl:col-span-7 min-h-96">
+                        <div className="w-full h-full bg-[#cd7c2e] hover:cursor-grab rounded-3xl overflow-hidden">
+                            <ContactExperience />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Contact
